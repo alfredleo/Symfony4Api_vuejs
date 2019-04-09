ba899b68de995736d8419e8c8b26d1869c426cce
Alfred <alfredleo@gmail.com>
2019-04-09 13:42:09 +0500

    7. Namespaces

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
+use App\Format\JSON;
+use App\Format as F;
+
 require __DIR__ . '/../vendor/autoload.php';
 print_r("Namespaces");
+
+
+$json = new JSON();
+$xml = new \App\Format\XML();
+$yml = new F\YAML();
+
+
+
+echo '<pre>';
+var_dump($json);
+var_dump($xml);
+var_dump($yml);
